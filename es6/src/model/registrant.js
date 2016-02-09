7a375d9a2df1b2d3e4172452538f469ec37479b5
Steven Kroh <sk.kroh@gmail.com>
2016-02-09 13:37:55 -0500

    Added shim.js to force istanbul to use all files
     - broken code from before the PouchDB era has been commented out

--- es6/src/model/registrant.js
+++ es6/src/model/registrant.js
@@ -1,64 +1,64 @@
-import { Collection } from 'backbone';
-import { ValidatedModel } from '../model/validated';
-import { User } from '../model/user';
-import { connect } from '../db/couch';
-
-export const Registrant = ValidatedModel.extend( {
-  url: function() {
-    return '/registrants/' + this.get( 'token' );
-  },
-
-  idAttribute: 'token',
-
-  validation: {
-    email: {
-      required: true,
-      pattern: 'email'
-    },
-    username: {
-      required: true
-    },
-    first: {
-      required: true
-    },
-    last: {
-      required: true
-    },
-    password: {
-      required: true // Need password requirements here later
-    }
-  },
-
-  getFields: function() {
-    return this.pick( [ 'email', 'password', 'first', 'last', 'username' ] );
-  },
-
-  confirm: function() {
-    var self = this;
-    return new Promise( ( resolve, reject ) => {
-      new User().save( this.getFields(), {
-        success: ( user, response, options ) => {
-          self.destroy();
-          resolve();
-        },
-        error: err => {
-          reject( err );
-        }
-      } );
-    } );
-  },
-
-  tokenize: function() {
-    return this.pick( 'email' );
-  }
-} );
-export const RegistrantCollection = Collection.extend( {
-  model: Registrant,
-  url: '/registrant'
-} );
-
-const couch = connect( 'registrants' );
-couch.install( err => {
-  Registrant.prototype.sync = couch.sync;
-  RegistrantCollection.prototype.sync = couch.sync;
-} );
+// import { Collection } from 'backbone';
+// import { ValidatedModel } from '../model/validated';
+// import { User } from '../model/user';
+// import { connect } from '../db/couch';
+//
+// export const Registrant = ValidatedModel.extend( {
+//   url: function() {
+//     return '/registrants/' + this.get( 'token' );
+//   },
+//
+//   idAttribute: 'token',
+//
+//   validation: {
+//     email: {
+//       required: true,
+//       pattern: 'email'
+//     },
+//     username: {
+//       required: true
+//     },
+//     first: {
+//       required: true
+//     },
+//     last: {
+//       required: true
+//     },
+//     password: {
+//       required: true // Need password requirements here later
+//     }
+//   },
+//
+//   getFields: function() {
+//     return this.pick( [ 'email', 'password', 'first', 'last', 'username' ] );
+//   },
+//
+//   confirm: function() {
+//     var self = this;
+//     return new Promise( ( resolve, reject ) => {
+//       new User().save( this.getFields(), {
+//         success: ( user, response, options ) => {
+//           self.destroy();
+//           resolve();
+//         },
+//         error: err => {
+//           reject( err );
+//         }
+//       } );
+//     } );
+//   },
+//
+//   tokenize: function() {
+//     return this.pick( 'email' );
+//   }
+// } );
+// export const RegistrantCollection = Collection.extend( {
+//   model: Registrant,
+//   url: '/registrant'
+// } );
+//
+// const couch = connect( 'registrants' );
+// couch.install( err => {
+//   Registrant.prototype.sync = couch.sync;
+//   RegistrantCollection.prototype.sync = couch.sync;
+// } );
